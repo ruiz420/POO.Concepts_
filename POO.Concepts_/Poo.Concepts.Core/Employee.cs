3b4d939019d78180f2255f402378ee8f5e9197f7
Your Name <roldanchakih@gmail.com>
2025-09-26 22:48:07 -0500

    clase astracta de employee

---
+++ POO.Concepts_/Poo.Concepts.Core/Employee.cs
@@ -0,0 +1,57 @@
+﻿using poo.concepts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo.Concepts.Core;
+
+public abstract class Employee
+{
+    public int Id
+    {
+        get;
+        set;
+    }
+
+    public string FirstName
+    {
+        get;
+        set;
+    } = null!;
+
+    public string LastName
+    {
+        get;
+        set;
+    } = null!;
+
+    public bool IsActive
+    {
+        get;
+        set;
+    }
+
+    }
+    public Date BornDate
+    { 
+        get; 
+        set; 
+    } = null!;
+
+    public Date HireDate 
+    { 
+        get; 
+        set; 
+    } = null!;
+
+    public abstract decimal GetValueToPay();
+
+    public override string ToString() => 
+        $"Id: {Id}\t{FirstName} {LastName}\n\t" +
+        $"HireDate: {HireDate}\n\t" +
+        $"Salary......: {GetValueToPay()}";
+    
+}
+
